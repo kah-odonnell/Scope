56c4ee5c0024b7f313e01057593752c297619295
Kyle O'Donnell <kah.odonnell@gmail.com>
2013-11-23 01:18:24 +0000

    first commit

---
+++ maps/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+"""
+class Event(models.Model):
+    event_title = models.CharField(max_length=255)
+    event_date = models.DateTimeField(auto_now_add=False)
+    longitute = models.DecimalField(max_digits=)
+"""
